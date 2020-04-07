b3614321da36c7124f76ecde9aa18a590e684ddb
Gavin Inglis <ginglis05@gmail.com>
2020-04-07 15:53:34 -0400

    add support for linux network

--- scripts/network.sh
+++ scripts/network.sh
@@ -7,11 +7,32 @@
 
 get_ssid()
 {
-	if /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep -E ' SSID' | cut -d ':' -f 2 &> /dev/null; then
-		echo "$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep -E ' SSID' | cut -d ':' -f 2)"
-	else
-		echo ' Ethernet'
-	fi
+	# Check OS
+	case $(uname -s) in
+		Linux)
+			if iw dev | grep ssid | cut -d ' ' -f 2 &> /dev/null; then
+				echo $(iw dev | grep ssid | cut -d ' ' -f 2)
+			else
+				echo ' Ethernet'
+			fi
+		;;
+
+		Darwin)
+			if /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep -E ' SSID' | cut -d ':' -f 2 &> /dev/null; then
+				echo "$(/System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I | grep -E ' SSID' | cut -d ':' -f 2)"
+			else
+				echo ' Ethernet'
+			fi
+		;;
+
+		CYGWIN*|MINGW32*|MSYS*|MINGW*)
+			# leaving empty - TODO - windows compatability
+		;;
+
+		*)
+		;;
+	esac
+
 }
 
 main()
