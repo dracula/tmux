52022a22a395dec672680f35e3d3a83c7f4ffdd0
Dane Williams <dwilli36@nd.edu>
2020-05-14 23:47:21 -0700

    fine tune after powerline PR

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -20,7 +20,8 @@ main()
   show_network=$(get_tmux_option "@dracula-show-network" true)
   show_weather=$(get_tmux_option "@dracula-show-weather" true)
   show_fahrenheit=$(get_tmux_option "@dracula-show-fahrenheit" true)
-  show_powerline=$(get_tmux_option "@dracula-show-powerline" true)
+  show_powerline=$(get_tmux_option "@dracula-show-powerline" false)
+
   # Dracula Color Pallette
   white='#f8f8f2'
   gray='#44475a'
