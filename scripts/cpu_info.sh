33703eb2d35b5612b9abd041c5e3f9f55fdc9b86
Gaganpreet <me@gaganpreet.in>
2020-08-31 11:41:45 +0200

    specify numeric locale for parsing top output

--- scripts/cpu_info.sh
+++ scripts/cpu_info.sh
@@ -4,7 +4,7 @@ get_percent()
 {
 	case $(uname -s) in
 		Linux)
-			percent=$(top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
+			percent=$(LC_NUMERIC=en_US.UTF-8 top -bn1 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{print 100 - $1"%"}')
 			echo $percent
 		;;
 		
