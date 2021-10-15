2e9b38e52474012bf1f28e0a0a3e7fe5a58a9e16
xiao0yy <xiao0yy@gmail.com>
2021-10-15 17:42:25 +0800

    rewrite network_bandwidth.sh with same style

--- scripts/network_bandwidth.sh
+++ scripts/network_bandwidth.sh
@@ -5,50 +5,74 @@ export LC_ALL=en_US.UTF-8
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $current_dir/utils.sh
 
-INTERVAL=$(get_tmux_option "@dracula-refresh-rate" 5)
+get_bytes()
+{
+  case $(uname -s) in
+    Linux)
+      download=$(cat /sys/class/net/$1/statistics/rx_bytes)
+      upload=$(cat /sys/class/net/$1/statistics/tx_bytes)
+      echo "$download $upload"
+      ;;
 
-default_network_name=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
-network_name=$(get_tmux_option "@dracula-network-bandwith" "$default_network_name")
+    Darwin)
+      # TODO - Darwin/Mac compatability
+      ;;
 
-main() {
-    output_download=""
-    output_upload=""
-    output_download_unit=""
-    output_upload_unit=""
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+      # TODO - windows compatability
+      ;;
+  esac
+}
 
-    initial_download=$(cat /sys/class/net/$network_name/statistics/rx_bytes)
-    initial_upload=$(cat /sys/class/net/$network_name/statistics/tx_bytes)
+get_bandwidth() {
+  output_download=""
+  output_upload=""
+  output_download_unit=""
+  output_upload_unit=""
 
-    sleep $INTERVAL
+  read initial_download initial_upload < <(get_bytes $2)
 
-    final_download=$(cat /sys/class/net/$network_name/statistics/rx_bytes)
-    final_upload=$(cat /sys/class/net/$network_name/statistics/tx_bytes)
+  sleep $1
 
-    total_download_bps=$(echo "$final_download $initial_download $INTERVAL" | awk '{printf "%.0f \n", ($1 - $2) / $3}')
-    total_upload_bps=$(echo "$final_upload $initial_upload $INTERVAL" | awk '{printf "%.0f \n", ($1 - $2) / $3}')
+  read final_download final_upload < <(get_bytes $2)
 
-    if [ $total_download_bps -gt 1073741824 ]; then
+  total_download_bps=$(echo "$final_download $initial_download $1" | awk '{printf "%.0f \n", ($1 - $2) / $3}')
+  total_upload_bps=$(echo "$final_upload $initial_upload $1" | awk '{printf "%.0f \n", ($1 - $2) / $3}')
+
+  if [ $total_download_bps -gt 1073741824 ]; then
       output_download=$(echo "$total_download_bps 1024" | awk '{printf "%.2f \n", $1/($2 * $2 * $2)}')
       output_download_unit="gB/s"
-    elif [ $total_download_bps -gt 1048576 ]; then
+  elif [ $total_download_bps -gt 1048576 ]; then
       output_download=$(echo "$total_download_bps 1024" | awk '{printf "%.2f \n", $1/($2 * $2)}')
       output_download_unit="mB/s"
-    else
+  else
       output_download=$(echo "$total_download_bps 1024" | awk '{printf "%.2f \n", $1/$2}')
       output_download_unit="kB/s"
-    fi
+  fi
 
-    if [ $total_upload_bps -gt 1073741824 ]; then
+  if [ $total_upload_bps -gt 1073741824 ]; then
       output_upload=$(echo "$total_download_bps 1024" | awk '{printf "%.2f \n", $1/($2 * $2 * $2)}')
       output_upload_unit="gB/s"
-    elif [ $total_upload_bps -gt 1048576 ]; then
+  elif [ $total_upload_bps -gt 1048576 ]; then
       output_upload=$(echo "$total_upload_bps 1024" | awk '{printf "%.2f \n", $1/($2 * $2)}')
       output_upload_unit="mB/s"
-    else
+  else
       output_upload=$(echo "$total_upload_bps 1024" | awk '{printf "%.2f \n", $1/$2}')
       output_upload_unit="kB/s"
-    fi
+  fi
 
-    echo "↓ $output_download $output_download_unit • ↑ $output_upload $output_upload_unit"
+  echo "↓ $output_download $output_download_unit • ↑ $output_upload $output_upload_unit"
 }
+
+main()
+{
+  # storing the refresh rate in the variable RATE, default is 5
+  RATE=$(get_tmux_option "@dracula-refresh-rate" 5)
+  default_network_name=$(ip route | grep default | sed -e "s/^.*dev.//" -e "s/.proto.*//")
+  network_name=$(get_tmux_option "@dracula-network-bandwith" "$default_network_name")
+  network_bandwidth=$(get_bandwidth "$RATE" "$network_name")
+  echo "$network_bandwidth"
+}
+
+# run the main driver
 main
