f4796bea7975c0ce15558b9eeeca3344cba641c7
Matt Crabtree <66781848+mj-crabtree@users.noreply.github.com>
2022-01-12 13:15:50 +0000

    utilised ram presents as a decimal value

--- scripts/ram_info.sh
+++ scripts/ram_info.sh
@@ -13,14 +13,14 @@ get_percent()
       used_mem=$(free -g | awk '/^Mem/ {print $3}')
       total_mem=$(free -h | awk '/^Mem/ {print $2}')
       if (( $total_mem_gb == 0)); then
-        memory_usage=$(free -m | awk '/^Mem/ {print $3}')
+        memory_usage=$(free -h | awk '/^Mem/ {print substr($3, 1, length($3) -2)}')
         total_mem_mb=$(free -m | awk '/^Mem/ {print $2}')
         echo $memory_usage\M\B/$total_mem_mb\M\B
       elif (( $used_mem == 0 )); then
-        memory_usage=$(free -m | awk '/^Mem/ {print $3}')
+        memory_usage=$(free -h | awk '/^Mem/ {print substr($3, 1, length($3) -2)}')
         echo $memory_usage\M\B/$total_mem_gb\G\B
       else
-        memory_usage=$(free -g | awk '/^Mem/ {print $3}')
+        memory_usage=$(free -h | awk '/^Mem/ {print substr($3, 1, length($3) -2)}')
         echo $memory_usage\G\B/$total_mem_gb\G\B
       fi
       ;;
