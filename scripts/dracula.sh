9a0bd01351c243ae11b63b0583a98ea9cb511942
jonathanforhan <jonathan.forhan@gmail.com>
2023-05-09 21:27:57 -0400

    colors

--- scripts/dracula.sh
+++ scripts/dracula.sh
@@ -239,7 +239,7 @@ main()
 
     # Window option
     if $show_powerline; then
-        tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${white},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"
+        tmux set-window-option -g window-status-current-format "#[fg=${gray},bg=${dark_purple}]${left_sep}#[fg=${dark_gray},bg=${dark_purple}] #I #W${current_flags} #[fg=${dark_purple},bg=${gray}]${left_sep}"
     else
         tmux set-window-option -g window-status-current-format "#[fg=${white},bg=${dark_purple}] #I #W${current_flags} "
     fi
