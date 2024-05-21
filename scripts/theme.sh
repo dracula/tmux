9131d6e27c0bcc7573064a99e264b4c80b5ae21d
Levi <leviszaboo@icloud.com>
2024-05-21 11:39:48 +0200

    feat: update themes

--- scripts/theme.sh
+++ scripts/theme.sh
@@ -2,29 +2,29 @@ set_theme() {
   case $1 in
     dragon)
       white=$old_white
-      gray=$sumi_ink_4
-      dark_gray=$sumi_ink_2
-      light_purple=$sumi_ink_5
-      dark_purple=$sumi_ink_6
-      cyan=$dragon_aqua
-      green=$dragon_green
-      orange=$dragon_orange
+      gray=$dragon_black_4
+      dark_gray=$dragon_black_2
+      light_purple=$dragon_orange
+      dark_purple=$dragon_black_5
+      cyan=$dragon_ash
+      green=$dragon_teal
+      orange=$dragon_yellow
       red=$dragon_red
-      pink=$dragon_pink
-      yellow=$ronin_yellow
+      pink=$dragon_orange
+      yellow=$dragon_yellow
       ;;
     lotus)
       white=$lotus_white_3
-      gray=$lotus_violet_1
+      gray=$lotus_yellow_2
       dark_gray=$lotus_white_3
-      light_purple=$lotus_violet_1
-      dark_purple=$lotus_violet_2
+      light_purple=$lotus_red_4
+      dark_purple=$lotus_red_4
       cyan=$lotus_cyan
-      green=$lotus_teal_2
-      orange=$lotus_red_2
-      red=$lotus_red_2
+      green=$lotus_red_2
+      orange=$lotus_aqua_2
+      red=$lotus_red_4
       pink=$lotus_pink
-      yellow=$lotus_yellow_2
+      yellow=$lotus_teal_3
       ;;
     *)
       white=$fuji_white
