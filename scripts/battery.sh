0d82c3a0684ffeb9b0a646f3cdda73ded37a7e95
ziraq young <ziraqyoung@outlook.com>
2026-01-04 10:33:50 +0300

    test!

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -2,6 +2,8 @@
 # setting the locale, some users have issues with different locales, this forces the correct one
 export LC_ALL=en_US.UTF-8
 
+# Temp comment
+
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $current_dir/utils.sh
 
